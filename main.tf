25f3c4f6d93295a73e5c65870f72517f9f03d325
peram-supriya <perampriya26@gmail.com>
2026-09-21 19:03:09 +0530

    actions

--- main.tf
+++ main.tf
@@ -118,7 +118,7 @@ resource "aws_route_table" "database" {
 #route 
 
 resource "aws_route" "public" {
-  route_table_id            = aws_route_table.public
+  route_table_id            = aws_route_table.public.id
   destination_cidr_block    = "0.0.0.0/0"
  gateway_id                 = aws_internet_gateway.main.id
 }
@@ -156,13 +156,13 @@ resource "aws_nat_gateway" "main" {
 }
 
 resource "aws_route" "private" {
-    route_table_id            = aws_route_table.private
+    route_table_id            = aws_route_table.private.id
     destination_cidr_block    = "0.0.0.0/0"
     nat_gateway_id            = aws_nat_gateway.main.id
 }
 
 resource "aws_route" "database" {
-    route_table_id            = aws_route_table.database
+    route_table_id            = aws_route_table.database.id
     destination_cidr_block    = "0.0.0.0/0"
     nat_gateway_id             = aws_nat_gateway.main.id
 }
