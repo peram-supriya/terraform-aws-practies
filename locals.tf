e74af92ae1ac2421453c8ff8dc7b9a4cb4a44719
peram-supriya <perampriya26@gmail.com>
2026-09-21 13:06:43 +0530

    actions

--- locals.tf
+++ locals.tf
@@ -1,8 +1,16 @@
 locals {
-    Project = var.project
-    Environment = var.environment
-
-    vpc_final_tags = {
-        Name = "$(local.Project)-$(local.Environment)"
+    comman_tags ={
+        Project = var.project
+        Environment = var.environment
+        Terraform = "true"
     }
+
+    vpc_final_tags = merge(
+        local.comman_tags,
+        
+        {
+        Name = "$(local.Project)-$(local.Environment)"
+    },
+    var.vpc_tags
+    )
 }
