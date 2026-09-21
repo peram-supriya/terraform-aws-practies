21bf4e959f78df24c3255d76bdfea9258deca455
peram-supriya <perampriya26@gmail.com>
2026-09-21 18:55:22 +0530

    actions

--- main.tf
+++ main.tf
@@ -114,3 +114,58 @@ resource "aws_route_table" "database" {
 
     )
 }
+
+#route 
+
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public
+  destination_cidr_block    = "0.0.0.0/0"
+ gateway_id                 = aws_internet_gateway.main.id
+}
+
+#elastic ip
+
+resource "aws_eip" "nat" {
+  domain                    = "vpc"
+  tags = merge(
+        local.comman_tags,
+        {
+            Name = "${var.project}-${var.environment}-nat"
+        },
+        var.eip_tags
+  )
+}
+
+#natgate way
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id  # we creating us-east-1a
+
+  tags = merge(
+        local.comman_tags,
+        {
+            Name = "${var.project}-${var.environment}"
+        },
+        var.nat_tags
+  )
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.main]
+}
+
+resource "aws_route" "private" {
+    route_table_id            = aws_route_table.private
+    destination_cidr_block    = "0.0.0.0/0"
+    nat_gateway_id            = aws_nat_gateway.main.id
+}
+
+resource "aws_route" "private" {
+    route_table_id            = aws_route_table.private
+    destination_cidr_block    = "0.0.0.0/0"
+    nat_gateway_id             = aws_nat_gateway.main.id
+}
+
+
+
--- variables.tf
+++ variables.tf
@@ -77,3 +77,13 @@ variable "database_route_table_tags" {
     type = map
   
 }
+
+variable "eip_tags" {
+    default = {}
+    type = map
+}
+
+variable "nat_tags" {
+    default = {}
+    type = map
+}
